388321c2edca8eecdeddd6711a8eb23b7d77903c
Your Name <abdullah.khan@devsinc.com>
2022-12-21 20:13:05 +0500

    Header Footer added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="flex justify-center px-5 bg-black text-white font-kanit">
-        <Home />
+      <div className="justify-center px-5 bg-black text-white font-kanit gap-4">
+        <Header/>
+        {/* <Home/> */}
+        <Footer/>
       </div>
     </div>
   );
